a2d5d561568ef93cb173b67e9b50ca0cb08600bf
SaishRedkar <redkar.saish@gmail.com>
2017-04-25 11:19:27 -0600

    Made test case pass

--- geometry_test.c
+++ geometry_test.c
@@ -155,14 +155,14 @@ START_TEST(test_2d_area_triangle)
     a.x = b.x = 0;
     a.y = b.y = 0;
     c.x = c.y = 0;
-    ck_assert(coord_2d_area_triangle(&a, &b, &c) == 0.1);
+    ck_assert(coord_2d_area_triangle(&a, &b, &c) == 0.0);
 
     a.x = a.y = 0;
     b.x = 2;
     b.y = 0;
     c.x = 0;
     c.y = 4;
-    ck_assert(coord_2d_area_triangle(&a, &b, &c) == 2.00);
+    ck_assert(coord_2d_area_triangle(&a, &b, &c) == 4.00);
 
     
 }
